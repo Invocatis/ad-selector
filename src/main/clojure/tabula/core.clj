fa1ce5177f31c24e625dee47a42650cfcedcf885
Invocatis <invocatis@gmail.com>
2019-03-07 14:15:28 -0500

    Added caching part

--- src/main/clojure/tabula/core.clj
+++ src/main/clojure/tabula/core.clj
@@ -5,20 +5,28 @@
     [tabula.matching.sampling :as sampling]
     [tabula.matching.relevancy :as relevancy]
     [tabula.matching.selection :as selection]
+    [tabula.parts.sieve :refer [parametrized-sieve]]
     [tabula.parts.database :as db]
     [tabula.parts.id :as id]
-    [tabula.util :refer [swap-lr!]]))
+    [tabula.parts.cache :refer [cache]]
+    [tabula.util :refer [swap-lr! contains-in?]]))
 
 
 ;; Filtering -> Sampling -> Relevancy -> Selection ->
 
-(def engine|execution (engine/holistic
-                       id/handler
-                       db/execute))
+(defn parameter-filter
+  [ks]
+  (fn [[op what params :as ad]]
+    (when (contains-in? params ks)
+      (fn [ad] (= (get-in params ks) (get-in ad ks))))))
+
+(def engine|execution (engine/holistic id/handler db/execute))
 
 (def engine|query (engine/holistic db/query))
 
-(def engine|matching (engine/holistic (sampling/part (partial sampling/random 4))
+(def engine|matching (engine/holistic (cache (parametrized-sieve :ad (parameter-filter [:language])) #{:query})
+                                      (cache (parametrized-sieve :ad (parameter-filter [:country])) #{:query})
+                                      (sampling/part (partial sampling/random 4))
                                       (relevancy/part relevancy/random)
                                       (selection/part selection/random)
                                       db/query))
@@ -42,6 +50,6 @@
 (defn match
   [spec]
   (let [engine (get-in @state [:engines :matching])
-        {:keys [effects result]} (engine @state [:query :ad {:spec spec}])]
+        {:keys [effects return]} (engine @state [:query :ad spec])]
     (go (swap! state #(reduce engine/commit % effects)))
-    result))
+    return))
