8b2c7f6a44e921bed5944d5a733b53803fb0da92
Invocatis <invocatis@gmail.com>
2019-03-07 11:16:21 -0500

    Added utility functions map-vals, filter-by-vals, and contains-in?

--- src/main/clojure/tabula/util.clj
+++ src/main/clojure/tabula/util.clj
@@ -8,3 +8,23 @@
       (if (compare-and-set! atom orig (left result orig))
         (right result)
         (recur)))))
+
+(defn map-vals
+  [f m]
+  (reduce (fn [m [k v]]
+            (conj m [k (f v)]))
+          {}
+          m))
+
+(defn filter-by-vals
+  [f m]
+  (reduce (fn [m [k v]]
+            (if (f v)
+              (conj m [k v])
+              m))
+          {}
+          m))
+
+(defn contains-in?
+  [m ks]
+  (contains? (get-in m (drop-last ks)) (last ks)))    
